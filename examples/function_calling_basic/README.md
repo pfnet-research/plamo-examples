91add2a31bc77389131574ebe0c10b4ae3cad18a
Shintarou Okada <okada@preferred.jp>
2024-08-30 18:46:58 +0900

    Add function calling example code

---
+++ examples/function_calling_basic/README.md
@@ -0,0 +1,35 @@
+Function Calling
+================
+
+指定したJSON Schemaに適合するようにJSONを出力させるサンプルです。
+
+「今日の東京の天気は？」というユーザの入力を元に、与えられた場所の今日の気温を返す
+
+```
+get_current_feather(location: string, unit: string)
+```
+
+のような関数を呼び出すための入力データを作成します。
+
+## 使い方
+
+まず、環境変数 `OPEAI_API_KEY` に、[PLaMo β版 トライアル](https://plamo.preferredai.jp/)で申し込みを行なって入手したAPIキーをセットしてください。
+
+```sh
+export OPENAI_API_KEY=<YOUR_API_KEY>
+```
+
+次に、必要なパッケージをインストールして、サンプルを実行します。
+
+```sh
+pip install -r requirements.txt
+
+python main.py
+```
+
+### 実行例
+
+```
+User input: 今日の東京の天気は？
+{ "location": "東京", "unit": "celsius" }
+```
---
+++ examples/function_calling_basic/main.py
@@ -0,0 +1,45 @@
+from openai import OpenAI
+client = OpenAI(
+    base_url="https://platform.preferredai.jp/api/completion/v1"
+)
+
+function = dict(
+    name="get_current_weather",
+    description="与えられた場所の現在の気温を返す",
+    parameters=dict(
+        type="object",
+        properties=dict(
+            location=dict(type="string", description="町の名前や地域の名前。例: 札幌市、北海道"),
+            unit=dict(type="string", enum=["celsius", "fahrenheit"]),
+        ),
+        required=["location"],
+    ),
+)
+
+tools = [
+    dict(type="function", function=function)
+]
+
+user_input = "今日の東京の気温は？"
+
+
+result = client.chat.completions.create(
+    model="plamo-beta",
+    messages=[
+        {"role": "user", "content": user_input},
+    ],
+    max_tokens=32,
+    temperature=0.1,
+    top_p=0.9,
+    n=1,
+    tools=tools,
+    tool_choice={
+        "type": "function",
+        "function": {
+            "name": "get_current_weather"
+        }
+    },
+)
+
+print(f"User input: {user_input}")
+print(result.choices[0].message.tool_calls[0].function.arguments)
