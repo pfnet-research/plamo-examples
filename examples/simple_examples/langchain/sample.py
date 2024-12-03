257a8eba44928451302c73461d777f5c016f1959
Shunta Saito <shunta.saito@gmail.com>
2024-12-03 19:05:30 +0900

    Update simple_examples/langchain

--- examples/simple_examples/langchain/sample.py
+++ examples/simple_examples/langchain/sample.py
@@ -1,13 +1,15 @@
 import os
+
 from langchain_openai import ChatOpenAI
 
 llm = ChatOpenAI(
-    base_url=f'https://platform.preferredai.jp/api/completion/v1',
-    model="plamo-beta",
+    base_url=f"https://platform.preferredai.jp/api/completion/v1",
+    model="plamo-1.0-prime",
+    api_key=os.environ["PLAMO_API_KEY"],
     # other params...,
 )
 
-messages=[
+messages = [
     {"role": "system", "content": "あなたは学校の先生です"},
     {"role": "user", "content": "二次方程式の解の公式を端的に教えてください"},
 ]
