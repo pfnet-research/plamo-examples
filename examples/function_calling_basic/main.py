16247de9ee0fe2df1ed63569571b607e4123bb79
Shunta Saito <shunta.saito@gmail.com>
2024-12-03 18:55:16 +0900

    Update function_calling_basic

--- examples/function_calling_basic/main.py
+++ examples/function_calling_basic/main.py
@@ -1,6 +1,10 @@
+import os
+
 from openai import OpenAI
+
 client = OpenAI(
-    base_url="https://platform.preferredai.jp/api/completion/v1"
+    base_url="https://platform.preferredai.jp/api/completion/v1",
+    api_key=os.environ["PLAMO_API_KEY"],
 )
 
 function = dict(
@@ -16,15 +20,13 @@ function = dict(
     ),
 )
 
-tools = [
-    dict(type="function", function=function)
-]
+tools = [dict(type="function", function=function)]
 
 user_input = "今日の東京の気温は？"
 
 
 result = client.chat.completions.create(
-    model="plamo-beta",
+    model="plamo-1.0-prime",
     messages=[
         {"role": "user", "content": user_input},
     ],
@@ -33,12 +35,7 @@ result = client.chat.completions.create(
     top_p=0.9,
     n=1,
     tools=tools,
-    tool_choice={
-        "type": "function",
-        "function": {
-            "name": "get_current_weather"
-        }
-    },
+    tool_choice={"type": "function", "function": {"name": "get_current_weather"}},
 )
 
 print(f"User input: {user_input}")
